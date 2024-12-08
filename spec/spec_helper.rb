19b717a110c9e8d65d29db4984f65cedeaf9192d
livdemirci <livdemirci@gmail.com>
2024-12-09 00:02:52 +0300

    formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,22 +6,24 @@
 
 # if use below, ci/rspec_reporter does not get output
 # only when run in BuildWise Agent, catpure output in result junit xml files
-if ENV["RUN_IN_BUILDWISE_AGENT"] == "true" 
+if ENV["RUN_IN_BUILDWISE_AGENT"] == "true"
   RSpec.configure do |config|
-    
     # register around filter that captures stdout and stderr
     config.around(:each) do |example|
-    
-        stdout, stderr = StringIO.new, StringIO.new
-        $stdout, $stderr = stdout, stderr
+      stdout, stderr = StringIO.new, StringIO.new
+      $stdout, $stderr = stdout, stderr
 
-        example.run
+      example.run
 
-        example.metadata[:stdout] = $stdout.string
-        example.metadata[:stderr] = $stderr.string
+      example.metadata[:stdout] = $stdout.string
+      example.metadata[:stderr] = $stderr.string
 
-        $stdout = STDOUT
-        $stderr = STDERR
+      $stdout = STDOUT
+      $stderr = STDERR
+    end
+
+    RSpec.configure do |config|
+      config.add_formatter 'AllureRspecFormatter'
     end
   end
-end  
+end
